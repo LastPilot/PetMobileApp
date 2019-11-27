477a00031e9cf76578fe56cbb32e93d25363f9d7
ccorra15 <christian.corrales.simontech16@gmail.com>
2019-11-27 11:54:51 -0800

    Created PetAdapter class which will take care of all the firebase actions such as retrieving and pushing data. Created the RetrieveData method which retrieves the data from the firebase.

---
+++ AndroidMagicGuild/app/src/main/java/com/example/androidmagicguild/Pet.kt
@@ -0,0 +1,14 @@
+package com.example.androidmagicguild
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+
+@IgnoreExtraProperties
+data class Pet(
+    val id : String? = "",
+    val name : String? = "",
+    val type : String? = "",
+    val bio : String? = "",
+    val contact : String? = "",
+    val zipCode : Int? = 0){
+}
