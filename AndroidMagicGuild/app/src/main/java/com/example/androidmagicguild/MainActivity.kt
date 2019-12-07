d7be26c6e73e6fa96fb8b6bb1972e8d0e2def9af
LastPilot <kristenmarenco@gmail.com>
2019-12-07 03:38:33 -0800

    added bootstrap to homepage to improve on UI style

--- AndroidMagicGuild/app/src/main/java/com/example/androidmagicguild/MainActivity.kt
+++ AndroidMagicGuild/app/src/main/java/com/example/androidmagicguild/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidmagicguild
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.beardedhen.androidbootstrap.TypefaceProvider
 import com.example.androidmagicguild.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        TypefaceProvider.registerDefaultIconSets();
         petAdapter.retrieveData()
         auth = FirebaseAuth.getInstance()
         auth.signInAnonymously()
